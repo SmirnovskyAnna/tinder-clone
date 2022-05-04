1e802e084b13cc5651388d4a3e1a8c6c90834298
SmirnovskyAnna <smirnova@appbooster.ru>
2022-05-04 21:07:28 +0300

    styled home page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,23 @@
-const Nav = () => {
-  return (
-    <div>
-      Nav
-    </div>
-  )
-}
+import styles from "./Nav.module.css";
+import whiteLogo from "../images/tinder_logo_white.png";
+import colorLogo from "../images/color-logo-tinder.png";
 
-export default Nav
+const Nav = ({ minimal, authToken }) => {
+  return (
+    <nav>
+      <div className={styles.logoContainer}>
+        <img
+          className={styles.logo}
+          src={minimal ? colorLogo : whiteLogo}
+          alt="Logo"
+        />
+      </div>
+
+      {!authToken && !minimal && (
+        <button className={styles.navButton}>Log in</button>
+      )}
+    </nav>
+  );
+};
+
+export default Nav;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,22 +2,22 @@ import Nav from "../components/Nav";
 import styles from "./Home.module.css";
 
 const Home = () => {
-  const authToken = true;
+  const authToken = false;
 
   const handleClick = () => {
     console.log("clicked");
   };
 
   return (
-    <>
-      <Nav />
+    <div className={styles.overlay}>
+      <Nav minimal={false} authToken={authToken} />
       <div className={styles.home}>
         <h1>Swipe Right</h1>
-        <button className={styles.primaryButton} onClick={handleClick}>
+        <button className="primaryButton" onClick={handleClick}>
           {authToken ? "Signout" : "Create Account"}
         </button>
       </div>
-    </>
+    </div>
   );
 };
 
