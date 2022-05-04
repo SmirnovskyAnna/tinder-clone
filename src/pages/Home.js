f41e914233942c3fcc7119be52bbbc5a0d585530
SmirnovskyAnna <smirnova@appbooster.ru>
2022-05-04 20:22:17 +0300

    created a layout of the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,24 @@
-const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
-}
+import Nav from "../components/Nav";
+import styles from "./Home.module.css";
 
-export default Home
+const Home = () => {
+  const authToken = true;
+
+  const handleClick = () => {
+    console.log("clicked");
+  };
+
+  return (
+    <>
+      <Nav />
+      <div className={styles.home}>
+        <h1>Swipe Right</h1>
+        <button className={styles.primaryButton} onClick={handleClick}>
+          {authToken ? "Signout" : "Create Account"}
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Home;
