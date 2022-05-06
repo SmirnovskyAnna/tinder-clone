d72a13676da4327c9d96d01dabb74b5925d1b21d
SmirnovskyAnna <smirnova@appbooster.ru>
2022-05-06 14:33:26 +0300

    refactored

--- server/index.js
+++ server/index.js
@@ -2,16 +2,16 @@ const PORT = 8000;
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const { MongoClient } = require("mongodb");
-const {v4: uuidv4} = require('uuid')
-const bcrypt = require('bcrypt')
-const cors = require('cors')
+const { v4: uuidv4 } = require("uuid");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
 
 const uri =
   "mongodb+srv://masav1997:Sa312520@cluster0.gljit.mongodb.net/Cluster0?retryWrites=true&w=majority";
 
 const app = express();
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json("Hello to my app!");
@@ -19,40 +19,42 @@ app.get("/", (req, res) => {
 
 app.post("/signup", async (req, res) => {
   const client = new MongoClient(uri);
-  const {email, password} = req.body
+  const { email, password } = req.body;
 
-  const generatedUserId = uuidv4()
-  const hashedPassword = await bcrypt.hash(password, 10)
+  const generatedUserId = uuidv4();
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   try {
-    await client.connect()
+    await client.connect();
     const database = client.db("app-data");
     const users = database.collection("users");
 
-    const exsistingUser = await users.findOne({email})
+    const exsistingUser = await users.findOne({ email });
 
     if (exsistingUser) {
-      return res.status(409).send('User already exsists. Please login.')
+      return res.status(409).send("User already exsists. Please login.");
     }
 
-    const sanitizedEmail = email.toLowerCase()
+    const sanitizedEmail = email.toLowerCase();
 
     const data = {
       user_id: generatedUserId,
       email: sanitizedEmail,
-      hashed_password: hashedPassword
-    }
-    const insertedUser = await users.insertOne(data)
+      hashed_password: hashedPassword,
+    };
+    const insertedUser = await users.insertOne(data);
 
     const token = jwt.sign(insertedUser, sanitizedEmail, {
       expiresIn: 60 * 24,
-    })
+    });
 
-    res.status(201).json({token, userId: generatedUserId, email: sanitizedEmail})
+    res
+      .status(201)
+      .json({ token, userId: generatedUserId, email: sanitizedEmail });
   } catch (err) {
-    console.log(err)
+    console.log(err);
   } finally {
-    await client.close()
+    await client.close();
   }
 });
 
